d9076bb6cadca6294d1a07c82bda19533f36d696
gorankovacic <goran.kovacic@thespian.hr>
2022-03-24 10:38:09 +0100

    Implement Navigation layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import Gallery from './Pages/Gallery';
+import Navigation from './Navigation/Navigation';
 
 export const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/" element={
-          <Home />
-        } />
-        <Route path="/gallery" element={
-          <Gallery />
-        } />
-      </Routes>
+      <Navigation />
+      <div className="main-content">
+        <Routes>
+          <Route exact path="/" element={
+            <Home />
+          } />
+          <Route path="/gallery" element={
+            <Gallery />
+          } />
+        </Routes>
+      </div>
     </Router>
   );
 };
---
+++ src/components/App/Navigation/Navigation.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../../assets/images/veritheum_logo.png';
+import NavigationFilter from '../NavigationFilter/NavigationFilter';
+import './Navigation.scss';
+
+class Navigation extends React.Component {
+  render () {
+    return (
+      <nav>
+        <div className="nav-items-wrapper">
+          <div className="item-group">
+            <div className="logo">
+              <img src={Logo} alt="Veritheum Logo" />
+            </div>
+            <NavigationFilter />
+          </div>
+          <div className="item-group">
+            <div className="links-wrapper">
+              <Link to="/">Home</Link>
+              <Link to="/gallery">Explore</Link>
+              <div className="group">
+                <Link to="/" className="outline">Create</Link>
+                <Link to="/" className="full">Sign in</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+};
+
+export default Navigation;
