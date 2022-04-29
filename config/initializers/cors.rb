45576f51953a100425d2fefc3c7b115d7922298a
Dario Dumlijan <dario.dumlijan1@gmail.com>
2022-04-29 14:30:13 +0200

    Added ORCID OAuth methods

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,11 +2,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   require 'dotenv/load'
-  FRONTEND_DEVELOPMENT = "http://localhost:#{ENV.fetch('FRONTEND_DEVELOPMENT_PORT', '3001')}"
 
   allow do
     # origins "*"
-    origins [FRONTEND_DEVELOPMENT]
+    origins [ENV.fetch('FRONTEND_BASE_URL', 'http://localhost:3001')]
     resource '*',
              expose: ['Authorization'],
              headers: :any,
