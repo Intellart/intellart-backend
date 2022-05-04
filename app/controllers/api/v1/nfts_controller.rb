9defbb3ae046e74bff4691412adad63642e985a8
Leon Kvez <leonkvez@gmail.com>
2022-05-04 09:55:56 +0200

    restrict action refresh_jwt for nfts

--- app/controllers/api/v1/nfts_controller.rb
+++ app/controllers/api/v1/nfts_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::NftsController < ApplicationController
   before_action :set_nft, only: [:show, :update, :destroy]
-  after_action :refresh_jwt
+  after_action :refresh_jwt, only: [:create, :update, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound do
     render_json_error :not_found, :nft_not_found
