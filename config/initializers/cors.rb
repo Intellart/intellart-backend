7afb8c17dc7cda36270ac0ffdb78628b11e7bf63
Leon Kvez <leonkvez@gmail.com>
2022-05-17 11:58:33 +0200

    setup basic websockets connection

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,10 +4,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   require 'dotenv/load'
 
   allow do
-    # origins "*"
-    origins [ENV.fetch('FRONTEND_BASE_URL', 'http://localhost:3001')]
+    origins '*'
+    # origins [ENV.fetch('FRONTEND_BASE_URL', 'http://localhost:3001')]
     resource '*',
-             expose: ['Authorization', '_jwt'],
+             expose: %w[Authorization _jwt],
              headers: :any,
              methods: %i[get post put patch delete head options]
   end
