d650a82a34432981bbde6cf147ab3869c761ecc0
gorankovacic <goran.kovacic@thespian.hr>
2022-05-19 08:18:33 +0200

    Implement Confirmation email layout

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,4 +2,11 @@ class UserMailer < Devise::Mailer
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
+
+  def confirmation_instructions(record, token, opts={})
+    @first_name = record.first_name
+    @last_name = record.last_name
+    @email = record.email
+    super
+  end
 end
