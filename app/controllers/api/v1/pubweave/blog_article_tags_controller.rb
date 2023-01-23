d4a470f32af6c7455aacd7cc23ee80961ef39d87
Leon Kvez <leonkvez@gmail.com>
2023-01-23 15:26:04 +0100

    Destroy action for blog article tags

--- app/controllers/api/v1/pubweave/blog_article_tags_controller.rb
+++ app/controllers/api/v1/pubweave/blog_article_tags_controller.rb
@@ -3,6 +3,8 @@ module Api
     module Pubweave
       class BlogArticleTagsController < ApplicationController
         skip_before_action :authenticate_api_user!, only: [:index]
+        before_action :require_owner, only: [:destroy]
+        before_action :set_article_tag, only: [:destroy]
 
         def index
           tags = BlogArticleTag.all
@@ -16,7 +18,20 @@ module Api
           render json: @article_tag, status: :created
         end
 
+        # DELETE api/v1/pubweave/blog_article_tags/:id
+        def destroy
+          render json: @article_tag.id, status: ok if @article_tag.destroy
+        end
+
         private
+
+        def set_article_tag
+          @article_tag = BlogArticleTag.find(params[:id])
+        end
+
+        def require_owner
+          head :unauthorized unless @current_user.id == @article_tag.blog_article.user_id
+        end
 
         def article_tags_params
           params.require(:blog_article_tag).permit(:tag_id, :blog_article_id, :category_id)
