43261af4ac230e0aee05222437f410c358e4776a
Leon Kvez <leonkvez@gmail.com>
2023-01-27 14:49:03 +0100

    Modify destroy comment action

--- app/controllers/api/v1/pubweave/blog_article_comments_controller.rb
+++ app/controllers/api/v1/pubweave/blog_article_comments_controller.rb
@@ -56,7 +56,7 @@ module Api
 
         # DELETE api/v1/pubweave/blog_article_comments/:id
         def destroy
-          head :no_content if @comment.destroy
+          render json: @comment.id, status: ok if @comment.destroy
         end
 
         private
