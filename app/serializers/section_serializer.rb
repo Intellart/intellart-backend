9be0053f5518dfa5f6ca988981c61b7efb8d4e42
dariobogovic <dario.bogovic@thespian.hr>
2023-06-07 13:43:32 +0200

    DB: added collaborator username to section serializer

--- app/serializers/section_serializer.rb
+++ app/serializers/section_serializer.rb
@@ -1,5 +1,9 @@
 class SectionSerializer < ActiveModel::Serializer
-  attributes :id, :type, :position, :data, :version_number, :collaborator_id
+  attributes :id, :type, :position, :data, :version_number, :collaborator_id, :collaborator
+
+  def collaborator
+    object.collaborator.username
+  end
 
   def id
     object.editor_section_id
