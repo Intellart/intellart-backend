13f19fb225464ec682b3c93db4bce62712fc0c9b
dariobogovic <dario.bogovic@thespian.hr>
2023-06-07 09:41:24 +0200

    DB: inline image params

--- app/helpers/articles_params_helper.rb
+++ app/helpers/articles_params_helper.rb
@@ -17,6 +17,10 @@ module ArticlesParamsHelper
     [:caption, :withBackground, :withBorder, :stretched, { file: [:url] }]
   end
 
+  def inline_image_params
+    [:caption, :withBackground, :withBorder, :stretched, :url, { usplash: [:author, :profileLink] }]
+  end
+
   def link_params
     # [:link]
   end
