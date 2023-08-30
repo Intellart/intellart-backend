f3855a229933b57ce99e151fc2b3c40df718471b
dariobogovic <dario.bogovic@thespian.hr>
2023-08-30 15:05:52 +0200

    Added TimeWithZone to config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Veritheum
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.active_record.yaml_column_permitted_classes = [Symbol, Date, Time, ActiveSupport::TimeWithZone, ActiveSupport::TimeZone]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
