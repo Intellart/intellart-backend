4156a05ec8b8f986410a5912bda003b7daf6ff54
mateo <mateo1559@gmail.com>
2024-01-08 03:20:00 +0100

    review_controller: added create/delete routes

--- app/controllers/api/v1/pubweave/reviews_controller.rb
+++ app/controllers/api/v1/pubweave/reviews_controller.rb
@@ -3,10 +3,52 @@ module Api
     module Pubweave
       class ReviewsController < ApplicationController
         def show
-          review = Review.where(id: params[:id])
-          render json: review, include: :user_reviews
+          review = Review.where(article_id: params[:id])
+          render json: review, include: { user_reviews: { serializer: UserReviewSerializer } }
         end
-        
+
+        def create
+          review = Review.new(review_params.except(:user_ids))
+          if review.save
+            # create all user_reviews
+            user_ids = review_params[:user_ids]
+            user_ids.each do |user_id|
+              user = User.find(user_id)
+              user_review = UserReview.new(user_id: user_id, review_id: review.id)
+              user_review.save
+            end
+
+            # switch article status to reviewing
+            article = Article.find(review_params[:article_id])
+            article.reviewing!
+
+            render json: review, include: { user_reviews: { serializer: UserReviewSerializer } },  status: :created
+          else
+            render json: review.errors, status: :unprocessable_entity
+          end
+        end
+
+        def destroy
+          review = Review.find(params[:id])
+          
+          review.user_reviews.each do |user_review|
+            user_review.destroy
+          end
+
+          
+
+          if review.destroy
+            render json: { id: params[:id] }
+          else
+            render json: { error: 'Failed to delete review' }, status: :unprocessable_entity
+          end
+        end
+
+        private
+
+        def review_params
+          params.require(:review).permit(:deadline, :article_id, :amount, user_ids: [])
+        end
       end
     end
   end
