02e15360cb58886c9c2ac2a2c6e0c312645bbcc1
feghalya <albert.feghaly@intellart.ca>
2025-02-16 11:28:44 -0500

    Add show method to ConfirmationsController

--- app/controllers/api/v1/confirmations_controller.rb
+++ app/controllers/api/v1/confirmations_controller.rb
@@ -3,6 +3,23 @@ module Api
     class ConfirmationsController < Devise::ConfirmationsController
       skip_before_action :authenticate_api_user!
 
+      def show
+        token = params.require(:confirmation_token)
+
+        if token
+          user = User.find_by(confirmation_token: token)
+          if user
+            # Proceed with confirmation
+            user.confirm
+            render json: { userID: user.id }, status: :ok
+          else
+            render json: { error: 'Invalid token' }, status: :unprocessable_entity
+          end
+        else
+          render json: { error: 'Missing confirmation token' }, status: :unprocessable_entity
+        end
+      end
+
       protected
 
       def after_confirmation_path_for(_resource_name, _resource)
